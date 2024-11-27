2572796f4e3ea2b56e82f0f80ef23fdae4e43d2e
John Elliott <160415536+Jakelliott98@users.noreply.github.com>
2024-11-27 18:59:33 +0000

    Fixed the dimension probelm in style.css with the container
    Used JS to fix the issue I was having with having the items as squares within the container.
    
    Used JS to set the dimensions for the item based on how many columns / rows have been input. Therefore, they always fit into the container exactly.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const gridContainer = document.getElementById('container');
 const resetButton = document.getElementById('resetButton');
 
-function createGrid(number){
+function createGrid(number, length){
+    let itemDimensions = `${650 / length}px`;
+    // Calculating item width & height to be equal a square. 
     for (let i = 0; i < number; i++){
         let item = document.createElement('div');
-        item.className = 'item';
         gridContainer.appendChild(item);
+        item.style.width = itemDimensions;
+        item.style.height = itemDimensions;
         item.addEventListener('mouseover', () => {
             item.style.backgroundColor = randomColor();
-        })
+        });
     };
 };
 
@@ -19,12 +22,12 @@ function randomColor(){
     return `rgb(${red}, ${green}, ${blue})`;
 };
 
-
 resetButton.addEventListener('click', () => {
     gridContainer.textContent = '';
     let number = prompt('Give me a number!');
     let rowNumber = number * number;
-    createGrid(rowNumber);
+    createGrid(rowNumber, number);
 });
 
-
+window.addEventListener('load', createGrid(256, 16));
+// Initial call to load 16 x 16 grid.
